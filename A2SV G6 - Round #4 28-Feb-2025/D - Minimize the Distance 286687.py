c63e49f3703f97aa1478b92fc9594fd18661f0fd
Ashenafi <ashumulugeta016@gmail.com>
2025-02-28 11:17:14 +0300

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 28-Feb-2025/D - Minimize the Distance 286687.py
@@ -0,0 +1,11 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+n = int(input())
+num = list(map(int,input().strip().split()))
+def sol(num,n):
+    num.sort()
+    val = (n-1)//2
+    return num[val]
+        
+    
+print(sol(num,n))
