1c7044ccf088b20174bb3fcf5600af1a30504319
Ashenafi <ashumulugeta016@gmail.com>
2025-03-08 23:14:18 +0300

    Created 'Implement Queue using Stacks' by A2SV Hub

---
+++ 08-Mar-2025/Implement Queue using Stacks 298238.py
@@ -0,0 +1,31 @@
+# Problem: Implement Queue using Stacks - https://leetcode.com/problems/implement-queue-using-stacks/
+
+class MyQueue:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+
+    def peek(self) -> int:
+        if self.queue:
+            return self.queue[0]
+
+    def empty(self) -> bool:
+        if self.queue:
+            return False
+        else:
+            return True
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
