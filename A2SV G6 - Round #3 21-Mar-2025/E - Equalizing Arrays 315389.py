3a9ab80955df09e6854087b44a539597010ea708
Ashenafi <ashumulugeta016@gmail.com>
2025-03-22 01:01:32 +0300

    Created 'E - Equalizing Arrays' by A2SV Hub

---
+++ A2SV G6 - Round #3 21-Mar-2025/E - Equalizing Arrays 315389.py
@@ -0,0 +1,39 @@
+# Problem: E - Equalizing Arrays - https://codeforces.com/gym/588468/problem/E
+
+n = int(input())
+num = list(map(int, input().strip().split()))
+x = int(input())
+val = list(map(int, input().strip().split()))
+def solutions(n,num,x,val):
+    if sum(val) != sum(num):
+        return -1
+    else:
+        one = []
+        two = []
+        l = 0
+        r = 0 
+        first = 0
+        second = 0
+        first += num[l]
+        second += val[r]
+        while l < n and r < x:
+                
+            if first == second:
+
+                one.append(first)
+                two.append(second)
+                l += 1
+                r += 1
+                if l < n and r < x:
+                    first += num[l]
+                    second += val[r]
+            elif first < second:
+                l += 1
+                first += num[l]
+            else:
+                r += 1
+                if r < x:
+                    second += val[r]
+        return len(one)
+
+print(solutions(n,num,x,val))
